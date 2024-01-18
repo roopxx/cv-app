bf625d2f447e1164a52544df10d096e123c1c1c0
roopxx <127219908+roopxx@users.noreply.github.com>
2024-01-18 13:58:03 +0530

    Add resumeData with useState hook to handleChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Personal from "./components/Personal";
+import Resume from "./components/Resume";
+import { useState } from "react";
 
 function App() {
+  const resumeData = {
+    firstName: "John",
+    lastName: "Doe",
+    email: "johndoe@email.com",
+    phoneNumber: "9876543210",
+    address: "Chennai, India",
+  };
+
+  const [personalDetails, setPersonalDetails] = useState(resumeData);
+
+  function handleChange(event) {
+    setPersonalDetails({
+      ...personalDetails,
+      [event.target.name]: event.target.value,
+    });
+  }
 
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
-
-  )
+    <div className="mx-auto w-4/5">
+      <div className="w-full">
+        <Header />
+      </div>
+      <div className="flex gap-6">
+        <div className="w-1/3">
+          <Personal
+            personalDetails={personalDetails}
+            handleChange={handleChange}
+          />
+        </div>
+        <div className="w-full">
+          <Resume personalDetails={personalDetails} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
