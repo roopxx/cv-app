17c3b7f4f04e3dce64e5c114204fb1e614865d70
roopxx <127219908+roopxx@users.noreply.github.com>
2024-01-19 13:07:17 +0530

    Create `<section>` to view educational info

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -15,6 +15,41 @@ const Resume = ({ personalDetails }) => {
           </p>
         </div>
       </section>
+      <section className="bg-white">
+        <div className="mx-10">
+          <div className="pt-8">
+            <h2 className="bg-slate-200 text-center">Education</h2>
+          </div>
+          <div className="flex space-x-10">
+            <div className="space-y-2 p-2">
+              <p>
+                {(() => {
+                  var [startDate, endDate] = [
+                    personalDetails.startDate,
+                    personalDetails.endDate,
+                  ];
+                  startDate = startDate
+                    ? Intl.DateTimeFormat("en-GB", {
+                        dateStyle: "short",
+                      }).format(new Date(personalDetails.startDate))
+                    : "";
+                  endDate = endDate
+                    ? Intl.DateTimeFormat("en-GB", {
+                        dateStyle: "short",
+                      }).format(new Date(personalDetails.endDate))
+                    : "";
+                  return `${startDate} - ${endDate}`;
+                })()}
+              </p>
+              <p>{personalDetails.location}</p>
+            </div>
+            <div className="space-y-2 p-2">
+              <p>{personalDetails.schoolName}</p>
+              <p>{personalDetails.degree}</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
