9a70c947e8c759f66a7cd4d593933b72867b419f
roopxx <127219908+roopxx@users.noreply.github.com>
2024-01-24 23:44:17 +0530

    Restructure Resume component with dynamic rendering

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,17 +1,22 @@
 import { PropTypes } from "prop-types";
 
-const Resume = ({ personalDetails }) => {
+const Resume = ({ resumeData }) => {
+  const [personal, educational, professional] = [
+    resumeData[0][0],
+    resumeData[1],
+    resumeData[2],
+  ];
   return (
     <div className="mt-10 border border-black">
       <section className="bg-cyan-950">
         <div className="py-10 text-center">
           <h1 className="mb-2 text-3xl text-white">
-            {personalDetails.firstName} {personalDetails.lastName}
+            {personal.firstName} {personal.lastName}
           </h1>
           <p className="flex justify-center gap-4 text-xl text-white">
-            <span>{personalDetails.email}</span>
-            <span>{personalDetails.phoneNumber}</span>
-            <span>{personalDetails.address}</span>
+            <span>{personal.email}</span>
+            <span>{personal.phoneNumber}</span>
+            <span>{personal.address}</span>
           </p>
         </div>
       </section>
@@ -20,39 +25,36 @@ const Resume = ({ personalDetails }) => {
           <div className="pt-8">
             <h2 className="bg-slate-200 text-center">Education</h2>
           </div>
-          <div className="flex w-full space-x-10">
-            <div className="w-1/4 space-y-2 p-2">
-              <p>
-                {(() => {
-                  var [startDate, endDate] = [
-                    personalDetails.startDate,
-                    personalDetails.endDate,
-                  ];
-                  startDate = startDate
-                    ? Intl.DateTimeFormat("en-GB", {
-                        dateStyle: "short",
-                      }).format(new Date(personalDetails.startDate))
-                    : "";
-                  endDate =
-                    endDate === ""
-                      ? ""
-                      : endDate ===
-                            Intl.DateTimeFormat("fr-CA").format(new Date()) ||
-                          endDate === "present"
-                        ? "present"
-                        : Intl.DateTimeFormat("en-GB", {
-                            dateStyle: "short",
-                          }).format(new Date(personalDetails.endDate));
-                  return `${startDate} - ${endDate}`;
-                })()}
-              </p>
-              <p>{personalDetails.location}</p>
+          {educational.map((info) => (
+            <div key={info.id}>
+              <div className="flex w-full space-x-10">
+                <div className="w-1/4 space-y-2 p-2">
+                  <p>
+                    {(() => {
+                      var [startDate, endDate] = [
+                        Intl.DateTimeFormat("en-GB", {
+                          dateStyle: "short",
+                        }).format(new Date(info.startDate)),
+                        Intl.DateTimeFormat("en-GB", {
+                          dateStyle: "short",
+                        }).format(new Date(info.endDate)),
+                      ];
+                      console.log("Start Date:", info.startDate);
+                      console.log("End Date:", info.endDate);
+
+                      return `${startDate} - ${endDate}`;
+                    })()}
+                  </p>
+                  <p>{info.location}</p>
+                </div>
+                <div className="w-3/4 space-y-2 p-2">
+                  <p>{info.schoolName}</p>
+                  <p>{info.degree}</p>
+                </div>
+              </div>
+              <hr className="border" />
             </div>
-            <div className="w-3/4 space-y-2 p-2">
-              <p>{personalDetails.schoolName}</p>
-              <p>{personalDetails.degree}</p>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
       <section className="bg-white">
@@ -62,40 +64,36 @@ const Resume = ({ personalDetails }) => {
               Professional Experience
             </h2>
           </div>
-          <div className="flex w-full space-x-10">
-            <div className="w-1/4 space-y-2 p-2">
-              <p>
-                {(() => {
-                  var [emp_startDate, emp_endDate] = [
-                    personalDetails.emp_startDate,
-                    personalDetails.emp_endDate,
-                  ];
-                  emp_startDate = emp_startDate
-                    ? Intl.DateTimeFormat("en-GB", {
-                        dateStyle: "short",
-                      }).format(new Date(personalDetails.emp_startDate))
-                    : "";
-                  emp_endDate =
-                    emp_endDate === ""
-                      ? ""
-                      : emp_endDate ===
-                            Intl.DateTimeFormat("fr-CA").format(new Date()) ||
-                          emp_endDate === "present"
-                        ? "present"
-                        : Intl.DateTimeFormat("en-GB", {
-                            dateStyle: "short",
-                          }).format(new Date(personalDetails.emp_endDate));
-                  return `${emp_startDate} - ${emp_endDate}`;
-                })()}
-              </p>
-              <p>{personalDetails.workLocation}</p>
+          {professional.map((info) => (
+            <div key={info.id}>
+              <div className="flex w-full space-x-10">
+                <div className="w-1/4 space-y-2 p-2">
+                  <p>
+                    {(() => {
+                      var [emp_startDate, emp_endDate] = [
+                        Intl.DateTimeFormat("en-GB", {
+                          dateStyle: "short",
+                        }).format(new Date(info.emp_startDate)),
+                        Intl.DateTimeFormat("en-GB", {
+                          dateStyle: "short",
+                        }).format(new Date(info.emp_endDate)),
+                      ];
+                      console.log("Start Date:", info.emp_startDate);
+                      console.log("End Date:", info.emp_endDate);
+
+                      return `${emp_startDate} - ${emp_endDate}`;
+                    })()}
+                  </p>
+                  <p>{info.workLocation}</p>
+                </div>
+                <div className="w-3/4 space-y-2 p-2">
+                  <p>{info.companyName}</p>
+                  <p>{info.position}</p>
+                  <p>{info.jobDescription}</p>
+                </div>
+              </div>
             </div>
-            <div className="w-3/4 space-y-2 p-2">
-              <p>{personalDetails.companyName}</p>
-              <p>{personalDetails.position}</p>
-              <p>{personalDetails.jobDescription}</p>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </div>
@@ -103,7 +101,7 @@ const Resume = ({ personalDetails }) => {
 };
 
 Resume.propTypes = {
-  personalDetails: PropTypes.object,
+  resumeData: PropTypes.array,
 };
 
 export default Resume;
